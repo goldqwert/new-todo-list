ae869f035b2eed25dc65cfaf5da714fc4d986afa
Yury <goldyukol12@gmail.com>
2020-01-30 19:41:43 +0200

    Updates

---
+++ precache-manifest.195e51aab007f51a604847db28545bf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ce59aab58555111c6c0f456edf07d3",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "9566d579a0cb82156dc9",
+    "url": "/TodoList/static/css/main.44dfaf9c.chunk.css"
+  },
+  {
+    "revision": "e107d4b0446ab25a8eb3",
+    "url": "/TodoList/static/js/2.f002fea0.chunk.js"
+  },
+  {
+    "revision": "9566d579a0cb82156dc9",
+    "url": "/TodoList/static/js/main.858e0418.chunk.js"
+  },
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  },
+  {
+    "revision": "096c3694e22c6e5bea58ea743f773b70",
+    "url": "/TodoList/static/media/Loader.096c3694.svg"
+  }
+]);
